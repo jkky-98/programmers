f2f7049b3281693c27b484640fa4e3c44b3e2324
JUNGI HONG <83509290+jkky-98@users.noreply.github.com>
2024-04-08 15:52:56 +0900

    [Silver I] Title: 음식물 피하기, Time: 112 ms, Memory: 37336 KB -BaekjoonHub

--- 백준/Silver/1743. 음식물 피하기/음식물 피하기.py
+++ 백준/Silver/1743. 음식물 피하기/음식물 피하기.py
@@ -1,55 +1,35 @@
-'''
-3 4 5
-3 2
-2 2
-3 1
-2 3
-1 1
-'''
-row, col, n = map(int, input().split())
+from queue import Queue
+from sys import stdin
+
+row, col, n = map(int, stdin.readline().split())
 
 map_lst = [[0] * (col) for _ in range(row)]
 visited = [[0] * (col) for _ in range(row)]
 
 for i in range(n):
-    x, y = map(int, input().split())
+    x, y = map(int, stdin.readline().split())
     x = x - 1
     y = y - 1
 
     map_lst[x][y] = 1
 
-from queue import Queue
 
 def search_tool(start_row, start_col, map_lst, visited):
     q = Queue()
     q.put((start_row, start_col))
     visited[start_row][start_col] = 1
     power = 1
+    can_go = [(-1,0),(1,0),(0,-1),(0,1)]
 
     while not q.qsize() == 0:
         row, col = q.get()
-
-        if 0 <= row - 1 < len(map_lst):
-            if visited[row - 1][col] == 0 and map_lst[row - 1][col] == 1:
-                q.put((row - 1, col))
-                visited[row - 1][col] = 1
-                power += 1
-        if 0 <= row + 1 < len(map_lst):
-            if visited[row + 1][col] == 0 and map_lst[row + 1][col] == 1:
-                q.put((row + 1, col))
-                visited[row + 1][col] = 1
-                power += 1
-        if 0 <= col - 1 < len(map_lst[0]):
-            if visited[row][col - 1] == 0 and map_lst[row][col - 1] == 1:
-                q.put((row, col - 1))
-                visited[row][col - 1] = 1
-                power += 1
-        if 0 <= col + 1 < len(map_lst[0]):
-            if visited[row][col + 1] == 0 and map_lst[row][col + 1] == 1:
-                q.put((row, col + 1))
-                visited[row][col + 1] = 1
-                power += 1
-    
+        
+        for i in can_go:
+            if 0 <= row + i[0] < len(map_lst) and 0 <= col + i[1] < len(map_lst[0]):
+                if visited[row + i[0]][col + i[1]] == 0 and map_lst[row + i[0]][col + i[1]]:
+                    q.put((row + i[0], col + i[1]))
+                    visited[row + i[0]][col + i[1]] = 1
+                    power += 1
     return visited, power
 
 power = 0

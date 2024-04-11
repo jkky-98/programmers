c1b2b562e9c8d56c045f2c3e3ddf2e8ba9e68ea4
JUNGI HONG <83509290+jkky-98@users.noreply.github.com>
2024-04-11 22:15:41 +0900

    [Gold V] Title: 센서, Time: 220 ms, Memory: 32656 KB -BaekjoonHub

---
+++ 백준/Gold/2212. 센서/센서.py
@@ -0,0 +1,23 @@
+from sys import stdin
+
+n_sensor = int(stdin.readline())
+n_house = int(stdin.readline())
+
+sensor_lst = list(map(int, stdin.readline().split()))
+# 중복제거
+sensor_lst = list(set(sensor_lst))
+# 정렬
+sensor_lst.sort()
+
+dist_lst = []
+for idx, i in enumerate(sensor_lst):
+    if idx + 1 < len(sensor_lst):
+        dist = sensor_lst[idx + 1] - i
+        dist_lst.append(dist)
+
+for i in range(n_house - 1):
+    if len(dist_lst) > 0:
+        dist_lst.remove(max(dist_lst))
+
+
+print(sum(dist_lst))

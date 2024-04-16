d8667a16218fd2665774df668790dd8d7736db9b
JUNGI HONG <83509290+jkky-98@users.noreply.github.com>
2024-04-16 18:29:04 +0900

    [Silver I] Title: 회전 초밥, Time: 96 ms, Memory: 34060 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.py
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.py
@@ -1,44 +1,36 @@
-
-
 from sys import stdin
-
+from collections import defaultdict
 N, d, k, c = map(int, stdin.readline().split())
 
 sushi = [int(stdin.readline()) for _ in range(N)]
 
-def two_part(front, k, N):
-    first_start = front
-    first_end = N - 1
+window = [sushi[x] for x in range(k)]
+sushi_dict = defaultdict(int)
+sushi_dict[c] = 1
 
-    second_start = 0
-    second_end = k - ((N - 1) - front) - 2
+def counting(sushi_dict):
+    count = len(sushi_dict)
+    return count
 
-    return first_start, first_end, second_start, second_end
-
-def analysis(sushi_lst, c):
-    unique = len(list(set(sushi_lst)))
-    if c not in sushi_lst:
-        unique += 1
-    return unique
+for i in window:
+    sushi_dict[i] += 1
     
-max_sushi = 0
-for i in range(N):
-    front = i
-    end = i + k - 1
-    if end >= N:
-        fs, fe, ss, se = two_part(front, k, N)
-        f_sushi = sushi[fs:fe+1]
-        s_sushi = sushi[ss:se+1]
-        f_sushi.extend(s_sushi)
-        sushi_tmp = f_sushi
-    
-    else:
-        sushi_tmp = sushi[front:end+1]
 
-    how_many_sushi = analysis(sushi_tmp, c)
-    max_sushi = max(max_sushi, how_many_sushi)
+max_sushi = counting(sushi_dict)
+
+for i in range(k,N+k-1):
+    end = i
+
+    if end > N-1:
+        end = (end % N)
+    
+    out_ = sushi[end-k]
+    in_ = sushi[end]
+    
+    sushi_dict[out_] -= 1
+    if sushi_dict[out_] == 0:
+        sushi_dict.pop(out_)
+    sushi_dict[in_] += 1
+    max_sushi = max(max_sushi, counting(sushi_dict))
 
 print(max_sushi)
-
-    
-    

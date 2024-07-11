986d1661a201aad4aa3e5d7eb6a290d211ea5b2f
JUNGI HONG <83509290+jkky-98@users.noreply.github.com>
2024-07-11 23:16:28 +0900

    [Bronze III] Title: 최소, 최대, Time: 544 ms, Memory: 118636 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,28 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+        List<Integer> list = new ArrayList<>();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            list.add(Integer.parseInt(st.nextToken()));
+        }
+
+        Integer min = Collections.min(list);
+        Integer max = Collections.max(list);
+
+        System.out.println(min + " " + max);
+    }
+}

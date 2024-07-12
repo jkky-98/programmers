f2a06d06bb42ee02109d3756655b1d47fdabd5c5
JUNGI HONG <83509290+jkky-98@users.noreply.github.com>
2024-07-12 10:47:33 +0900

    [Bronze V] Title: A+B - 5, Time: 108 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,23 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            String[] s = br.readLine().split(" ");
+            int A = Integer.parseInt(s[0]);
+            int B = Integer.parseInt(s[1]);
+
+            if (A == 0 && B == 0) {
+                break;
+            }
+
+            System.out.println(A + B);
+        }
+    }
+}

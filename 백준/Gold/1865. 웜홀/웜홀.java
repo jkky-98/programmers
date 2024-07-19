9114b0ee6ea148d6a54dff3075df4204997ce1d5
JUNGI HONG <83509290+jkky-98@users.noreply.github.com>
2024-07-19 21:48:31 +0900

    [Gold III] Title: 웜홀, Time: 524 ms, Memory: 64188 KB -BaekjoonHub

--- 백준/Gold/1865. 웜홀/웜홀.java
+++ 백준/Gold/1865. 웜홀/웜홀.java
@@ -1,113 +1,117 @@
-import java.io.*;
 
-import java.util.Arrays;
-import java.util.StringTokenizer;
-import java.util.ArrayList;
- 
+import java.io.*;
+import java.util.*;
+
 public class Main {
-    static int N, M, W;
-    static int[] dist;
-    static ArrayList<ArrayList<Road>> a;
-    static final int INF = 10000001;
-    
-    public static class Road {
-    int end;
-    int weight;
- 
-    Road(int end, int weight) {
-        this.end = end;
-        this.weight = weight;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static final int INF_VALUE = 9000001;
+
+    public static class Edge {
+        int start;
+        int end;
+        int exp;
+
+        public Edge(int start, int end, int exp) {
+            this.start = start;
+            this.end = end;
+            this.exp = exp;
+        }
+
+        @Override
+        public String toString() {
+            return "Edge{" +
+                    "start=" + start +
+                    ", end=" + end +
+                    ", exp=" + exp +
+                    '}';
         }
     }
- 
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
- 
         int TC = Integer.parseInt(br.readLine());
+
+        int[] dp;
+        Map<Integer,List<Edge>> map;
         StringBuilder sb = new StringBuilder();
-        while (TC-- > 0) {
+
+        for (int i = 0; i < TC; i++) {
             st = new StringTokenizer(br.readLine());
-            N = Integer.parseInt(st.nextToken());
-            M = Integer.parseInt(st.nextToken());
-            W = Integer.parseInt(st.nextToken());
- 
-            dist = new int[N + 1];
-            a = new ArrayList<>();
-            for (int i = 0; i <= N; i++) {
-                a.add(new ArrayList<>());
+            int N = Integer.parseInt(st.nextToken()); // 지점 수
+            int M = Integer.parseInt(st.nextToken()); // 도로 개수
+            int W = Integer.parseInt(st.nextToken()); // 웜홀 개수
+            // 메모리 초기화
+            map = new HashMap<>();
+            dp = new int[N+1];
+            for (int j = 1; j <= N; j++) {
+                map.put(j, new ArrayList<>());
             }
- 
-            for (int i = 0; i < M + W; i++) {
+            // 데이터 초기화
+            for (int j = 0; j < M; j++) {
                 st = new StringTokenizer(br.readLine());
                 int start = Integer.parseInt(st.nextToken());
                 int end = Integer.parseInt(st.nextToken());
-                int weight = Integer.parseInt(st.nextToken());
- 
-                if (i < M) { // 도로는 양방향 그래프
-                    a.get(start).add(new Road(end, weight));
-                    a.get(end).add(new Road(start, weight));
-                } else { // 웜홀은 단방향 그래프
-                    a.get(start).add(new Road(end, -weight));
-                }
+                int exp = Integer.parseInt(st.nextToken());
+                map.get(start).add(new Edge(start, end, exp));
+                map.get(end).add(new Edge(end, start, exp));
+
             }
- 
-            boolean isMinusCycle = false;
-            for (int i = 1; i <= N; i++) {
-                if (bellmanFord(i)) {
-                    isMinusCycle = true;
-                    sb.append("YES\n");
-                    break;
-                }
+
+            for (int j = 0; j < W; j++) {
+                st = new StringTokenizer(br.readLine());
+                int start = Integer.parseInt(st.nextToken());
+                int end = Integer.parseInt(st.nextToken());
+                int exp = -Integer.parseInt(st.nextToken());
+                map.get(start).add(new Edge(end, start, exp));
             }
- 
-            if (!isMinusCycle) {
-                sb.append("NO\n");
-            }
+
+            sb.append(bellmanford(map, dp, N) ? "YES\n" : "NO\n");
+
         }
- 
         bw.write(sb.toString());
         bw.flush();
         bw.close();
         br.close();
     }
-    
 
-    public static boolean bellmanFord(int start) {
-        Arrays.fill(dist, INF);
-        dist[start] = 0;
-        boolean update = false;
-        
+    private static boolean bellmanford(Map<Integer,List<Edge>> map, int[] dp, int N) {
 
-        for (int i = 1; i < N; i++) {
-            update = false;
-            
+        for (int i = 1; i <= N; i++) {
+            dp[i] = INF_VALUE;
+        }
+        boolean updated = false;
+
+        dp[1] = 0; // 시작점 설정
+
+        // N-1 반복
+        for (int i = 0; i < N-1; i++) {
+            updated = false;
+            // 모든 노드 조사
             for (int j = 1; j <= N; j++) {
-                for (Road road : a.get(j)) {
-                    if (dist[j] != INF && dist[road.end] > dist[j] + road.weight) {
-                        dist[road.end] = dist[j] + road.weight;
-                        update = true;
+                for (Edge edge : map.get(j)) {
+                    if (dp[edge.end] > dp[edge.start] + edge.exp) {
+                        updated = true;
+                        dp[edge.end] = dp[edge.start] + edge.exp;
                     }
                 }
             }
-            
-            if (!update) {
+
+            if (!updated) {
                 break;
             }
         }
-        
-        if (update) {
-            for (int i = 1; i <= N; i++) {
-                for (Road road : a.get(i)) {
-                    if (dist[i] != INF && dist[road.end] > dist[i] + road.weight) {
+
+        if (updated) {
+            for (int j = 1; j <= N; j++) {
+                for (Edge edge : map.get(j)) {
+                    if (dp[edge.end] > dp[edge.start] + edge.exp) {
                         return true;
                     }
                 }
             }
         }
- 
         return false;
+
     }
- 
 }

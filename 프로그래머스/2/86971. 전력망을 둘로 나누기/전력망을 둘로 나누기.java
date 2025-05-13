cd4ec0f4e0c412794be5af5507c8c363b1fa7901
JUNGI HONG <83509290+jkky-98@users.noreply.github.com>
2025-05-13 15:13:03 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 13.27 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,80 +1,81 @@
 import java.util.*;
 
 class Solution {
-    
-    public static List<Set<Integer>> nodes;
-    public static boolean[] visited;
-    public static Deque<Integer> queue;
-    public static List<Integer> resultList;
-    
     public int solution(int n, int[][] wires) {
-        initNodes(n);
-        visited = new boolean[n + 1];
-        queue = new ArrayDeque<>();
-        resultList = new ArrayList<>();
+        List<Set<Integer>> nodeInfos = new ArrayList<>();
+        List<Integer> resultList = new ArrayList<>();
+        // 노드 생성
+        nodeInfos.add(new HashSet<>());
         
-        for (int[] wire : wires) {
-            link(wire);
+        for (int i = 0; i < n; i++) {
+            nodeInfos.add(new HashSet<>());
         }
-        // wire 하나씩 끊어보며 송전탑 개수 차이 계산 wire복구 필수
-        for (int[] wire : wires) {
-            unlink(wire);
-            // BFS 서칭 -> 송전탑 개수 차이 판단
-            int etopDiff = Math.abs(bfs(wire[0]) - bfs(wire[1]));
-            resultList.add(etopDiff);
-            link(wire);
+        // wires 정보 삽입
+        for (int i = 0; i < wires.length; i++) {
+            int start = wires[i][0];
+            int end = wires[i][1];
+            
+            linkNodeInfo(start, end, nodeInfos);
         }
         
+        // wires 하나씩 끊기
+        for (int[] wire : wires) {
+            int start = wire[0];
+            int end = wire[1];
+            
+            unlinkNodeInfo(start, end, nodeInfos);
+            boolean[] visited = new boolean[n+1];
+            // start BFS
+            int startCount = 0;
+            Deque<Integer> queue = new ArrayDeque<>();
+            queue.offer(start);
+            visited[start] = true;
+            
+            while (!queue.isEmpty()) {
+                int startTmp = queue.poll();
+                Set<Integer> startNode = nodeInfos.get(startTmp);
+                
+                for (int node : startNode) {
+                    if (!visited[node]) {
+                        visited[node] = true;
+                        startCount++;
+                        queue.offer(node);
+                    }
+                } 
+            }
+            // end BFS
+            int endCount = 0;
+            queue.clear();
+            queue.offer(end);
+            visited[end] = true;
+            
+            while (!queue.isEmpty()) {
+                int endTmp = queue.poll();
+                Set<Integer> endNode = nodeInfos.get(endTmp);
+                
+                for (int node : endNode) {
+                    if (!visited[node]) {
+                        visited[node] = true;
+                        endCount++;
+                        queue.offer(node);
+                    }
+                } 
+            }
+            // 양쪽 결과 차이 쓰기
+            resultList.add(Math.abs(startCount - endCount));
+            // 복구
+            linkNodeInfo(start, end, nodeInfos);
+        }
         return Collections.min(resultList);
     }
-    public static int bfs(int start) {
-        // 초기화
-        int count = 0;
-        queue.clear();
-        Arrays.fill(visited, false);
-        
-        queue.offer(start);
-        visited[start] = true;
-        
-        while (!queue.isEmpty()) {
-            int link = queue.poll();
-            Set<Integer> node = nodes.get(link);
-            
-            for (int linkFromNode : node) {
-                if (!visited[linkFromNode]) {
-                    // queue에 link넣기 전에 처리 작업
-                    count++;
-                    visited[linkFromNode] = true;
-                    // queue에 link넣기
-                    queue.offer(linkFromNode);
-                }
-            }
-        }
-        
-        return count;
+    
+    public static void linkNodeInfo(int start, int end, List<Set<Integer>> nodeInfos) {
+        nodeInfos.get(start).add(end);
+        nodeInfos.get(end).add(start);
     }
     
-    public static void unlink(int[] wire) {
-        int start = wire[0];
-        int end = wire[1];
-        
-        nodes.get(start).remove(end);
-        nodes.get(end).remove(start);
-    }
-    
-    
-    public static void link(int[] wire) {
-        int start = wire[0];
-        int end = wire[1];
-
-        nodes.get(start).add(end);
-        nodes.get(end).add(start);
-    }
-    
-    public static void initNodes(int n) {
-        nodes = new ArrayList<>();
-        for (int i = 0; i <= n; i++) {
-            nodes.add(new HashSet<>());
-        }
+    public static void unlinkNodeInfo(int start, int end, List<Set<Integer>> nodeInfos) {
+        nodeInfos.get(start).remove(end);
+        nodeInfos.get(end).remove(start);
     }
 }

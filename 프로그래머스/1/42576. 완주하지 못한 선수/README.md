d9a32a649a8458c3782688529c39d6e6cbcb35ce
JUNGI HONG <83509290+jkky-98@users.noreply.github.com>
2025-06-16 15:59:29 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 70.62 ms, Memory: 97.7 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
 
 ### 성능 요약
 
-메모리: 96.6 MB, 시간: 106.28 ms
+메모리: 97.7 MB, 시간: 70.62 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 11일 16:20:55
+2025년 06월 16일 15:59:26
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,40 +1,43 @@
 import java.util.*;
 
 class Solution {
+    
     static Map<String, Integer> map;
     
     public String solution(String[] participant, String[] completion) {
-        
+        // map 초기화
         map = new HashMap<>();
         
-        setParticipantMap(participant);
-        return getFail(completion);
-    }
-    
-    public static void setParticipantMap(String[] participant) {
-        for (int i = 0; i < participant.length; i++) {
-            if (!map.containsKey(participant[i])) {
-                map.put(participant[i], 1);
+        // map에 completion 정보 넣기
+        for (String c : completion) {
+            int cValue = map.getOrDefault(c, 0);
+            
+            if (cValue == 0) {
+                map.put(c, 1);
             } else {
-                map.put(participant[i], map.get(participant[i]) + 1);
+                map.put(c, cValue + 1);
             }
-        }
-    }
-    
-    public static String getFail(String[] completion) {
-        for (int i = 0; i < completion.length; i++) {
-            map.put(completion[i], map.get(completion[i]) - 1);
-            
-            if (map.get(completion[i]).equals(0)) {
-                map.remove(completion[i]);
-            }
-            
         }
         
-        for (String key : map.keySet()) {
-            return key;
+        // participant 순회하며 map안의 정보 줄여나가기, 만약 정보가 없는데 줄일려고 하면 그녀석이 정답!
+        for (String p : participant) {
+            
+            int pValue = map.getOrDefault(p, 0);
+            
+            if (pValue == 0) {
+                return p;
+            }
+            
+            // 소거 로직
+            if (pValue == 1) {
+                map.remove(p);
+            } else {
+                map.put(p, pValue-1);
+            }
         }
         
         return null;
     }
+    // 여기서 알아가야할 점.
+    // map의 메서드를 알 수가 있다. getOrDefault(key, defaultValue), 이거랑 remove(key)
 }
